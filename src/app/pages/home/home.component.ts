6d6748f2d59b4be5c47e1fc2be2b0da97567fe1f
jimmy cruz <cruzperezjimmya@gmail.com>
2023-01-04 14:08:05 -0500

    avance de formulario, nota: mejorar alerta de validaciones

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,17 +1,51 @@
-import { Component } from '@angular/core';
-import { FormGroup, FormBuilder } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
   public formLogin!: FormGroup;
   nombre!:string;
+  email!:string;
+  mensaje!:string;
+
+  
 
   constructor(private formBuilder: FormBuilder){
-    
+
   }
+  ngOnInit(): void {
+    // throw new Error('Method not implemented.');
+
+    this.formLogin = this.formBuilder.group({
+      nombre:['',
+        [
+          Validators.required,
+          Validators.minLength(10)
+          // Validators
+        ]      
+      ],
+      email:['',
+        [
+          Validators.required,
+          Validators.email
+        ]
+      ],
+      mensaje:['',
+        [
+          Validators.required
+        ]
+      ],
+    })
+
+  }
+
+  send():any{
+    console.log(this.formLogin.value);
+  }
+
 
 }
